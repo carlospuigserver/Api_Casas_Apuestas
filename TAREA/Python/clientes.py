432d84a37c41e714022d2d8739a20b4b9d8f6c49
carlospuigserver <cpuiggut@myuax.com>
2024-02-04 14:56:28 +0100

    c

--- TAREA/Python/clientes.py
+++ TAREA/Python/clientes.py
@@ -1,5 +1,6 @@
 import sqlite3
 import pandas as pd
+import matplotlib.pyplot as plt
 conn = sqlite3.connect('bookmaker.db')
 
 # Ejecutar la consulta y cargar los datos en un DataFrame
@@ -11,3 +12,23 @@ df_clientes.to_csv('TAREA/CSV/clientes.csv', index=False)  # index=False para no
 conn.close()
 
 # Ahora df_clientes contiene tus datos y puedes comenzar tu análisis
+df_clientes['dominio_email'] = df_clientes['email'].apply(lambda x: x.split('@')[1])
+df_clientes['dominio_email'].value_counts().plot(kind='bar')
+plt.title('Frecuencia de Dominios de Correo Electrónico')
+plt.xlabel('Dominio')
+plt.ylabel('Frecuencia')
+plt.show()
+
+# Contar cuántos clientes están activados
+print("Clientes activados:", df_clientes['activado'].sum())
+
+
+
+# Gráfico de barras para mostrar el número de clientes activados vs no activados
+plt.figure(figsize=(6,4))
+df_clientes['activado'].value_counts().plot(kind='bar')
+plt.title('Distribución de Clientes Activados y No Activados')
+plt.xlabel('Estado de Activación')
+plt.ylabel('Cantidad de Clientes')
+plt.xticks(ticks=[0, 1], labels=['Activados', 'No Activados'], rotation=0)
+plt.show()
